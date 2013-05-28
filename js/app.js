b98ddf27f7a5d70a03d1700ea5f454b55aa03062
trejder <tomasz.trejderowski@us.edu.pl>
2013-05-28 15:52:16 +0200

    Add on application exit handler (with confirmation) plus fix really small menu issue.

--- js/app.js
+++ js/app.js
@@ -7,12 +7,32 @@ var app =
 	
     deviceReadyHandler: function()
 	{
+        document.addEventListener("backbutton", app.backButtonHandler, true);
+        
 		setTimeout(function()
 		{
 			navigator.splashscreen.hide();
 		},
 		1000);
-    }
+    },
+	
+    backButtonHandler: function()
+	{
+		//Don't force programmatic application exit ever or your app won't be published to Apple Store!
+        navigator.notification.confirm
+		(
+            'Are you sure, you want to exit?', //Message body.
+            app.onExitConfirm, //Callback after button is tapped.
+            'Exit application?', //Message title.
+            'Exit,Stay'//Buttons -- Will be shown in reversed order!
+        );
+    },
+	
+	onExitConfirm: function(button)
+	{
+		//Don't force programmatic application exit ever or your app won't be published to Apple Store!
+		if (button == 1) navigator.app.exitApp();
+	}
 };
 
 app.init();
